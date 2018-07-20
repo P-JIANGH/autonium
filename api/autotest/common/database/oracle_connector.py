b7885931ec31dc424e434a20bbc7f4d817264521
P-JIANGH <P_JIANGH@163.com>
2018-07-20 11:42:49 +0800

    finish oracle connector

--- api/autotest/common/database/oracle_connector.py
+++ api/autotest/common/database/oracle_connector.py
@@ -9,11 +9,14 @@ Oracle数据库连接器
 __author__ = 'JIANGH'
 
 import jpype, os
-from jpype import JavaException
+from jpype import java, JavaException, JClass, JPackage
 from . import _logger
 from ..config_reader import readconfig
 # 执行jar位置路径
 jar_path = readconfig('database', 'jarpath')
+
+def log_java_exception(exception):
+  _logger.error('\n'.join([line for line in exception.stacktrace().splitlines() if not line == '']))
 
 class OracleConn(object):
   """
@@ -29,17 +32,23 @@ class OracleConn(object):
     # 拼接URL
     url = "jdbc:oracle:thin:{user}/{password}@{host}:{database}".format(**kwargs)
     # 开启java虚拟机
-    jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % jar_path)
-    # 获取驱动类
-    OracleConnector = jpype.JClass('cn.com.transcosmos.OracleConnector')
-    # 传入URL并实例化
-    self.connector = OracleConnector(url)
-    # 调用connect方法
-    self.connector.connect()
+    jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % jar_path, "-Djdbc.drivers=oracle.jdbc.OracleDriver")
+    DriverManager = java.sql.DriverManager
+    try:
+      self.connector = DriverManager.getConnection(url)
+      self.connector.setAutoCommit(False)
+    except JavaException as e:
+      _logger.error('Oracle Driver: Cannot Connected to the DataBase')
+      log_java_exception(e)
+      self.connector = None
+      raise e
+    else:
+      _logger.info('Oracle Driver: Connect Success')
 
   # 关闭连接
   def close(self):
-    self.connector.close()
+    if self.connector:
+      self.connector.close()
 
   # TODO 未实现
   def execute(self, sql, params=None):
@@ -73,38 +82,129 @@ class OracleConn(object):
     """向表中插入数据
     位置参数：表名，列名列表，数据表格（二维列表）
     """
-    columns_list_ = jpype.java.util.ArrayList()
-    columns_list_.addAll(columns)
+    # 测试请求数据库
+    testSql = 'select {cols} from "{table_name}" where rownum = 1'.format(
+      cols=', '.join(columns), table_name=table_name
+    )
+    # 获取目标表的列定义
+    try:
+      preStat = self.connector.prepareStatement(testSql)
+      testResult = preStat.executeQuery()
+      columnsTypeList = [testResult.getMetaData().getColumnType(jpype.JInt(i + 1)) for i in range(len(columns))]
+    except JavaException as e:
+      _logger.error('Oracle Driver: Error In Execute Test SQL')
+      log_java_exception(e)
+      self.close()
+      raise e
 
-    _data_list = jpype.java.util.ArrayList()
-    _data_list_item = jpype.java.util.ArrayList()
-    for item in datalist:
-      _data_list_item.addAll(item)
-      _data_list.add(_data_list_item)
-      _data_list_item = jpype.java.util.ArrayList()
+    # 生成SQL模板
+    insert_sql_template = 'insert into "{table_name}" ({cols}) values ({placeholders})'.format(
+      table_name=table_name, cols=', '.join(columns), placeholders=', '.join(['? ' for i in range(len(columns))])
+    )
+    result = 0
+    try:
+      for row in datalist:
+        preStat = self.connector.prepareStatement(insert_sql_template)
+        paramList = []
+        for i, data in enumerate(row):
+          if data == None:
+            preStat.setNull(jpype.JInt(i + 1), columnsTypeList[i])
+            paramList.append(None)
+          else:
+            param_class = self.__switchType(columnsTypeList[i])
+            param = self.__prepareParam(param_class, data)
+            if param_class.getSimpleName() == "Integer":
+              methodName = 'setInt'
+            else:
+              methodName = 'set' + param_class.getSimpleName()
+            getattr(preStat, methodName)(jpype.JInt(i + 1), param)
+            paramList.append(str(param._pyv))
+        _logger.info("\nExecute: " + insert_sql_template + "\nParams: " + ', '.join(paramList))
+        result = result + preStat.executeUpdate()
+    except JavaException as e:
+      _logger.error("Oracle Driver: SQL Error in execute insert")
+      log_java_exception(e)
+      self.connector.rollback()
+      self.close()
+      raise e
+    else:
+      self.connector.commit()
+      _logger.info("Oracle Driver: Insert %d rows into %s" % (result, table_name))
 
-    _logger.info("Insert %d rows." % self.connector.insert(table_name, columns_list_, _data_list))
+  def __switchType(self, code):
+    Types = java.sql.Types
+    if (code == Types.VARCHAR or
+      code == Types.CHAR or
+      code == Types.NCHAR or
+      code == Types.NVARCHAR or
+      code == Types.LONGNVARCHAR or
+      code == Types.LONGVARCHAR):
+      return java.lang.Class.forName('java.lang.String')
+    elif code == Types.INTEGER:
+      return java.lang.Class.forName('java.lang.Integer')
+    elif code == Types.BIGINT:
+      return java.lang.Class.forName('java.lang.Long')
+    elif code == Types.BOOLEAN:
+      return java.lang.Class.forName('java.lang.Boolean')
+    elif code == Types.NUMERIC or code == Types.DECIMAL:
+      return java.lang.Class.forName('java.math.BigDecimal')
+    elif code == Types.DOUBLE:
+      return java.lang.Class.forName('java.lang.Double')
+    elif code == Types.FLOAT:
+      return java.lang.Class.forName('java.lang.Float')
+    elif code == Types.DATE:
+      return java.lang.Class.forName('java.sql.Date')
+    elif code == Types.TIME or code == Types.TIME_WITH_TIMEZONE:
+      return java.lang.Class.forName('java.sql.Time')
+    elif code == Types.TIMESTAMP or code == Types.TIMESTAMP_WITH_TIMEZONE:
+      return java.lang.Class.forName('java.sql.Timestamp')
+    else:
+      return java.lang.Class.forName('java.lang.String')
+
+  def __prepareParam(self, param_class, data):
+    if data == None: return None
+    try:
+      if param_class.getSimpleName() == 'BigDecimal':
+        return java.math.BigDecimal(jpype.JString(str(data)))
+      elif param_class.getSimpleName() == 'String':
+        return jpype.JString(str(data))
+      elif param_class.getSimpleName() == 'Integer':
+        return java.lang.Integer(jpype.JString(str(data))).intValue()
+      else:
+        return param_class.getMethod('valueOf', java.lang.Class.forName('java.lang.String')).invoke(None, jpype.JString(str(data)))
+    except JavaException as e:
+      _logger.error("Oracle Driver: SQL Error in prepare Params")
+      log_java_exception(e)
+      self.close()
+      raise e
 
   def select_with_header(self, sql):
     '''选取数据并返回表头
     返回值1: 数据表（二维列表）
     返回值2: 表头列表'''
-    _result_list_map = list(self.connector.select(sql))
-    if len(_result_list_map) > 0:
-      key_list = list(_result_list_map[0].keySet().toArray())
-
-    result_list = []
-    for result_map in _result_list_map:
-      result_row = []
-      for key in key_list:
-        result_row.append(result_map.get(key))
-      result_list.append(result_row)
-    return result_list, key_list
+    pass
 
   def select(self, sql):
-    return self.select_with_header(sql)[0]
+    result_list = []
+    try:
+      preStat = self.connector.prepareStatement(sql)
+      query_result = preStat.executeQuery()
+      columnCounts = query_result.getMetaData().getColumnCount()
+      while columnCounts > 0 and query_result.next():
+        result_list_item = []
+        for i in range(columnCounts):
+          result_list_item.append(query_result.getObject(jpype.JInt(i + 1))._pyv)
+        result_list.append(result_list_item)
+    except JavaException as e:
+      _logger.error("Oracle Driver: SQL Error in Query Select")
+      log_java_exception(e)
+      self.close()
+      raise e
+    else:
+      return result_list
 
   # 删除对象实例时关闭数据库连接，并关闭java虚拟机
   def __del__(self):
     self.close()
-    jpype.shutdownJVM()
+    if jpype.isJVMStarted:
+      jpype.shutdownJVM()
