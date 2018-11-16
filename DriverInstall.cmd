298127cff91d8c314d71d676844822f13073016f
P-JIANGH <P_JIANGH@163.com>
2018-11-16 13:39:51 +0800

    add x64 Driver

--- DriverInstall.cmd
+++ DriverInstall.cmd
@@ -11,9 +11,12 @@ pip install django-cors-headers
 REM Database Driver
 pip install psycopg2
 pip install .\database_driver\install_package\mysqlclient-1.3.13-cp36-cp36m-win32.whl
+pip install .\database_driver\install_package\mysqlclient-1.3.13-cp37-cp37m-win_amd64.whl
 pip install .\database_driver\install_package\pymssql-2.1.3-cp36-cp36m-win32.whl
+pip install .\database_driver\install_package\pymssql-2.1.4-cp37-cp37m-win_amd64.whl
 REM install JVM driver
 pip install .\database_driver\install_package\JPype1-0.6.3-cp36-cp36m-win32.whl
+pip install .\database_driver\install_package\JPype1-0.6.3-cp37-cp37m-win_amd64.whl
 REM Excel
 pip install xlwings
 pip install Pillow
